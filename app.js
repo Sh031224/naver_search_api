9bb8b5403daee8c2dde03580d381a8c848007667
Sh031224 <1cktmdgh2@gmail.com>
2020-06-15 20:53:29 +0900

    [FIX] Port Change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 const blog = require("./routes/blog");
 const news = require("./routes/news");
 
